90395d8c5e9296933796e69a6f8b54574e1c775d
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-12-03 15:35:36 -0500

    implemented tests for action creators

--- testing/src/store/__tests__/comments.test.js
+++ testing/src/store/__tests__/comments.test.js
@@ -1,15 +1,30 @@
 import commentsReducer from '../reducers/comments'
+import * as actions from '../actions/comments'
 import { SAVE_COMMENT } from '../actions/types'
 
-const COMMENT = 'Hello world'
+describe('saveComment', () => {
+  const COMMENT = 'Hello world'
 
-it('should handle actions of type SAVE_COMMENT', () => {
-  const action = {
-    type: SAVE_COMMENT,
-    payload: COMMENT
-  }
+  it('should handle actions of type SAVE_COMMENT', () => {
+    const action = {
+      type: SAVE_COMMENT,
+      payload: COMMENT
+    }
 
-  const newState = commentsReducer([], action)
+    const newState = commentsReducer([], action)
 
-  expect(newState).toEqual([COMMENT])
+    expect(newState).toEqual([COMMENT])
+  })
+
+  it('should have the correct type', () => {
+    const action = actions.saveComment()
+
+    expect(action.type).toEqual(SAVE_COMMENT)
+  })
+
+  it('should have the correct payload', () => {
+    const action = actions.saveComment(COMMENT)
+
+    expect(action.payload).toEqual(COMMENT)
+  })
 })
