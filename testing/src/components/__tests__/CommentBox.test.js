9e6baef4fdfd46a9848abc5550a6293a2b43b54e
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-12-03 14:24:38 -0500

    implemented CommentBox tests

--- testing/src/components/__tests__/CommentBox.test.js
+++ testing/src/components/__tests__/CommentBox.test.js
@@ -18,9 +18,24 @@ it('should have a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1)
 })
 
-it('should have a textarea that users can type in', () => {
-  wrapped.find('textarea').simulate('change', { 
-    target: { value: 'Hello world!' } 
+describe('the text area', () => {
+  const comment = {
+    target: {
+      value: 'Hello world!'
+    }
+  }
+  
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', comment)
+    wrapped.update()
   })
-  wrapped.update()
+  it('should have a textarea that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual(comment.target.value)
+  })
+
+  it('should empty the textarea when form is submited', () => {
+    wrapped.find('form').simulate('submit')
+    wrapped.update()
+    expect(wrapped.find('textarea').prop('value')).toEqual('')
+  })
 })
