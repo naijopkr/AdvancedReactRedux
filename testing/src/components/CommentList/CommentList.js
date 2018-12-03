12e448c94c28be9af35fd07a425b98ee84967044
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-12-03 15:44:03 -0500

    implemented CommentList

--- testing/src/components/CommentList/CommentList.js
+++ testing/src/components/CommentList/CommentList.js
@@ -1,3 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default () => <div>Comment List</div>
+class CommentList extends Component {
+  renderComments = () => {
+    const commentList = this.props.comments.map((comment, index) => {
+      return (
+        <li key={index}>
+          {comment}
+        </li>
+      )
+    })
+
+    return commentList
+  }
+  
+  render = () => {
+    return (
+      <div>
+        <ul>
+          {this.renderComments()}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  comments: state.comments
+})
+
+export default connect(mapStateToProps)(CommentList)
