190104488570f34475ff4c18fee20dcd7789eccc
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-12-09 18:26:43 -0500

    created router

--- authAPI/index.js
+++ authAPI/index.js
@@ -1,14 +1,27 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const router = require('./router')
+const mongoose = require('mongoose')
+const http = require('http')
+
 const app = express()
 
+//DB Setup
+const mongooseConfig = {
+  useNewUrlParser: true,
+  useCreateIndex: true
+}
+mongoose.connect('mongodb://localhost:auth/auth', mongooseConfig)
+ 
 //App Setup
 app.use(morgan('combined'))
 app.use(bodyParser.json())
+router(app)
 
 //Server Setup
 const port = process.env.PORT || 5000
-app.listen(port, () => {
-  console.log('Server up and listening to port', port)
+const server = http.createServer(app)
+server.listen(port, () => {
+  console.log('Server is up!')
 })
