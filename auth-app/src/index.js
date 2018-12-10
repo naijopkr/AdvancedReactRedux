dc550286392154170eb2acecd2efb6b3c11ffc94
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-12-10 18:14:41 -0500

    wired up redux

--- auth-app/src/index.js
+++ auth-app/src/index.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import reducers from './store/reducers'
 
 import App from './components/App'
 import Welcome from './components/cover/Welcome'
 import SignIn from './components/auth/SignIn'
 import SignUp from './components/auth/SignUp'
 
+const store = createStore(reducers, {})
+
 const app = (
-  <BrowserRouter>
-    <App>
-      <Route path='/' exact component={Welcome} />
-      <Route path='/signup' component={SignUp} />
-      <Route path='/signin' component={SignIn} />
-    </App>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App>
+        <Route path='/' exact component={Welcome} />
+        <Route path='/signup' component={SignUp} />
+        <Route path='/signin' component={SignIn} />
+      </App>
+    </BrowserRouter>
+  </Provider>
 )
 
 ReactDOM.render(app, document.querySelector('#root'))
