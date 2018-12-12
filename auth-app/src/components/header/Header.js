e41e3740cc46c0db7d25e2f39c62008e5f3e242c
Ariel Barcellos <barcellos.ariel@gmail.com>
2018-12-11 19:31:15 -0500

    some css in header

--- auth-app/src/components/header/Header.js
+++ auth-app/src/components/header/Header.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import './Header.css'
 
-const header = () => {
-  return (
-    <div>
-      <Link to='/'>Redux Auth</Link>
-      <Link to='/signup'>Sign Up</Link>
-      <Link to='/signin'>Sign In</Link>
+const header = props => {
+  const links = props.auth
+  ? (
+    <>
       <Link to='/signout'>Sign Out</Link>
       <Link to='/feature'>Feature</Link>
+    </>
+  ) : (
+    <>
+      <Link to='/signup'>Sign Up</Link>
+      <Link to='/signin'>Sign In</Link>
+    </>
+  )
+
+  return (
+    <div className='header'>
+      <Link to='/'>Redux Auth</Link>
+      {links}
     </div>
   )
 }
 
-export default header
+const mapStateToProps = state => ({
+  auth: state.auth.authenticated
+})
+
+export default connect(mapStateToProps)(header)
